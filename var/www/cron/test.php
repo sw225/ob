27d3797cdc8e0ba07509ee247a4121e4154a88e1
kotaro sawa <sawa@f-i-d.jp>
2018-09-22 23:07:44 +0900

    log処理を追加

--- var/www/cron/test.php
+++ var/www/cron/test.php
@@ -4,15 +4,3 @@ require_once('Mail.php');
 $dir = '/var/www/';
 require_once $dir.'cron/send.php';
 
-mailsend(array(
-    'to'          => "sawa@f-i-d.jp", 
-    'from'        => "ysn.kg.jpn@gmail.com",
-    'from_mk'     => "test",
-    'from_def'    => "", 
-    'smtp_server' => "smtp.gmail.com",
-    'smtp_port'   => "587",
-    'user'        => "ysn.kg.jpn@gmail.com",
-    'pass'        => "__Localhost2525",
-    'title'       => "プログラムテスト送信",
-    'message'     => "アイウエオ"
-));
